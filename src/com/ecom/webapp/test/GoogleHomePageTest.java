3ea47dcb1f3257204ed4a05cdba682ff961e745a
Sofia Nayak <nayaksofia1989@gmail.com>
2021-05-09 18:50:23 +0530

    Changes committed

---
+++ src/com/ecom/webapp/test/GoogleHomePageTest.java
@@ -0,0 +1,49 @@
+package com.ecom.webapp.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleHomePageTest {
+
+	public static void main(String[] args) {
+	
+		//1. Formulate A Base Test URL
+        final  String siteURL = "https://www.google.com/";
+        
+		//2. Locate A Web Driver BY Locating the path
+        
+         
+        final String driverPath = "C:\\Users\\nayak\\Desktop\\simplilearn\\Phase_5_FSD\\FSD-5 WorkSpace-Practice-Projects\\SeleniumChromeDriverTestOne\\driver\\chromedriver.exe";
+        
+        
+        //3. Set Selenium System Properties.
+        
+         System.setProperty("webdriver.chrome.driver",driverPath);
+		
+         //4. Instantiate Selenium Web Driver . That means Create an object of the web driver.
+        
+             WebDriver driver = new ChromeDriver();
+         
+   
+         
+		//5. Launch Browser
+         
+         driver.get(siteURL);
+
+		//6. Perform Test Evaluation 
+         
+        String expectedTitle = "Google";
+        
+        if (expectedTitle.equals(driver.getTitle())) {
+       	 System.out.println("Test is Passed !");
+        }else {
+       	 System.out.println("Test is Failed !");
+        }
+        
+		//7. Close Driver 
+         driver.close();
+
+
+	}
+
+}
